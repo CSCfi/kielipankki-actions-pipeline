a13eb287d923458bed54318e99d90caa197e231a
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-08-30 07:55:00 +0000

    KP-6009 Make runners ephemeral
    This way we don't need to worry so much about cleanup.

--- runner/bin/runner-setup.sh
+++ runner/bin/runner-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export RUNNER_ALLOW_RUNASROOT="1"
-./config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --token $1
+./config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --token $1
 ./run.sh

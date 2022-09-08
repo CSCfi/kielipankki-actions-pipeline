091a7ec481d47ad764795d84fe1d3e4f3886b00a
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-09-08 10:01:08 +0000

    KP-5973 Do unattended configuration with authorization token
    Trying to configure a runner without `--unttended` fails in pod due to
    the script determining that it cannot write to terminal. For some reason
    the unattended configuration disables the option to use a personal
    access token though, so we must fetch an authorization token from the
    GitHub API first and then use it when creating the runner.

--- runner/bin/runner-setup.sh
+++ runner/bin/runner-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export RUNNER_ALLOW_RUNASROOT="1"
-./config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --token $1
+HEADER="Authorization: Bearer $1"
+TOKEN_RESPONSE=$(curl -X POST -H "Accept: application/vnd.github+json" -H "$HEADER" https://api.github.com/repos/CSCfi/kielipankki-nlf-harvester/actions/runners/registration-token)
+TOKEN=$(echo $TOKEN_RESPONSE | jq --raw-output ".token")
+
+./config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --unattended --token $TOKEN
 ./run.sh

1980aa323dd2802f35f5fca1869d76fd56a24599
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2023-06-30 13:32:02 +0000

    KP-7378 Separate runner files into their own directory
    This makes the home directory neater and easier to navigate in case of
    having to download/build/install different things in the home directory.

--- runners/python-runner/bin/runner-setup.sh
+++ runners/python-runner/bin/runner-setup.sh
@@ -5,5 +5,5 @@ HEADER="$(echo "Authorization: Bearer $1" | tr -d '\n')"
 TOKEN_RESPONSE=$(wget --method=post -O- -q --header="Accept: application/vnd.github+json" --header="$HEADER" https://api.github.com/repos/CSCfi/kielipankki-nlf-harvester/actions/runners/registration-token)
 TOKEN=$(echo $TOKEN_RESPONSE | jq --raw-output ".token")
 
-./config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --unattended --token $TOKEN
-./run.sh
+/home/kprunner/runner/config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --unattended --token $TOKEN
+/home/kprunner/runner/run.sh

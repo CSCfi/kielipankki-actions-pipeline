272207942c785b96a62f5a337be9e23dc71d58ad
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2023-07-20 10:37:29 +0000

    KP-7378 Unify `runner-setup.sh` between runners
    The file contents are now identical, with the configured repository read
    from an environment variable. It would be ideal to actually share a
    file, but unfortunately Docker is not willing to include files from
    parent directories of the dockerfile, so this does not seem to be
    possible.

--- runners/python-runner/bin/runner-setup.sh
+++ runners/python-runner/bin/runner-setup.sh
@@ -2,8 +2,8 @@
 
 HEADER="$(echo "Authorization: Bearer $1" | tr -d '\n')"
 
-TOKEN_RESPONSE=$(wget --method=post -O- -q --header="Accept: application/vnd.github+json" --header="$HEADER" https://api.github.com/repos/CSCfi/kielipankki-nlf-harvester/actions/runners/registration-token)
+TOKEN_RESPONSE=$(wget --method=post -O- -q --header="Accept: application/vnd.github+json" --header="$HEADER" https://api.github.com/repos/$RUNNER_REPO/actions/runners/registration-token)
 TOKEN=$(echo $TOKEN_RESPONSE | jq --raw-output ".token")
 
-/home/kprunner/runner/config.sh --url https://github.com/CSCfi/kielipankki-nlf-harvester --ephemeral --unattended --token $TOKEN
+/home/kprunner/runner/config.sh --url https://github.com/$RUNNER_REPO --ephemeral --unattended --token $TOKEN
 /home/kprunner/runner/run.sh
